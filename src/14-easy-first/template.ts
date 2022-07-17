3c4bfe019e81341449ab4ec0ce7427e1fe849a2b
wynn-w <wwwynn22@gmail.com>
2022-07-17 23:50:07 +0800

    feat(14): add new solution is based on the judgment of the length of the array

--- src/14-easy-first/template.ts
+++ src/14-easy-first/template.ts
@@ -7,8 +7,15 @@
  *        本题考点：
  *              1. 如何做判断
  * */
-//  option 1
+// // option 1
 // type First<T extends unknown[]> = T[0] extends T[number] ? T[0] : never;
 
-//  * option 2
-type First<T extends unknown[]> = T extends [infer Frist, ...infer _Rest] ? Frist : never;
+// // * option 2
+// type First<T extends unknown[]> = T extends [infer Frist, ...infer _Rest] ? Frist : never;
+
+// ============== 数组的长度 ============== 
+// // option 3
+type First<T extends unknown[]> = T extends {length: 0} ? never : T[0];
+
+// // option 4
+// type First<T extends unknown[]> = T extends [] ? never : T[0];
