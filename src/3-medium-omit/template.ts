636f36ad153008f5bb5217b74ffdb4a72d193a8a
wynn-w <wwwynn22@gmail.com>
2022-07-27 23:56:09 +0800

    feat: complete challenge 3

---
+++ src/3-medium-omit/template.ts
@@ -0,0 +1,10 @@
+/**
+ * @notes 解题思路：
+ *                1. 能遍历所有属性
+ *                2. 将遍历的属性与传入属性做对比
+ *        本题考点：
+ *                1. as 做类型断言，承接左右两侧
+ * */
+type MyOmit<T, K extends keyof T> = {
+  [P in keyof T as P extends K ? never : P]: T[P];
+};
