a651ac71f1c5756e53b82b5957aa48a29c983ada
wynn-w <wwwynn22@gmail.com>
2022-08-12 01:05:16 +0800

    perf: use infer keyword

--- src/10-medium-tuple-to-union/template.ts
+++ src/10-medium-tuple-to-union/template.ts
@@ -1,5 +1,8 @@
 /**
  * @notes 解题思路：
  *                1. 利用数组下标一定是数字的特性
+ *                2. 利用 infer 递归套取以一个元素
+ *                   => 注意三元表达式为真时的处理
  * */
-type TupleToUnion<T extends unknown[]> = T[number];
+// type TupleToUnion<T extends unknown[]> = T[number];
+type TupleToUnion<T extends unknown[]> = T extends [infer First, ...infer Rest] ? First | TupleToUnion<Rest> : never;
