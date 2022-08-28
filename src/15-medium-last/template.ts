2d01efbec9872ed09659ec470e0c1134bd75b167
wynn-w <wwwynn22@gmail.com>
2022-08-28 23:26:10 +0800

    perf(15): get last item by array length

--- src/15-medium-last/template.ts
+++ src/15-medium-last/template.ts
@@ -1,1 +1,9 @@
-type Last<T extends any[]> = T extends [...infer P, infer Last] ? Last : never;
+/**
+ * @notes 解题思路：
+ *              1. 拓展符 + infer
+ *              2. 利用数字的长度
+ *        知识点：
+ *              1. T['length'] 可以获取数组长度
+ * */
+// type Last<T extends any[]> = T extends [...infer _P, infer Last] ? Last : never;
+type Last<T extends any[]> = [never,...T][T['length']];
